f1d8f0068f274c6c17d0bf930c70a145dcb1a6e6
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-17 15:45:55 +0100

    add second snack

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function getInitials(arr) {
+    const arrInitials = [];
+    for (let i = 0; i < arr.length; i++) {
+        arrInitials[i] = arr[i].slice(0, 1)
+    }
+    return arrInitials
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(getInitials(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
