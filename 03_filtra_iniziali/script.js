b74f9c71153946eb2ad8ade376728f49df94b8eb
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-17 16:08:17 +0100

    add snack 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-
+function getFilteredByInitials(arr, initial) {
+    const arrFilterNames = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].slice(0, 1) === initial) {
+            arrFilterNames.push(arr[i])
+        }
+    }
+    return arrFilterNames
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(getFilteredByInitials(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
