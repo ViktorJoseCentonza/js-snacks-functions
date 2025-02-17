c491cf863aa75236c3d5f0f70fe4042a82e7c933
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-17 16:18:40 +0100

    add snack 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,21 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
 
-
+function vowelCounter(str) {
+    const vowels = "aeiouAEIOU";
+    let numberOfVowels = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            numberOfVowels++
+        }
+    }
+    return numberOfVowels
+}
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(vowelCounter(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
